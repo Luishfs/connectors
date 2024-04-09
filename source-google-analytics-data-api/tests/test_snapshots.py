fd663e17014fda9f3ea623313b34c2d8e0b0d2b8
Luishfs <luish1980@hotmail.com>
2024-04-09 04:15:31 -0300

    Added capture snapshots and fixed pk order

--- source-google-analytics-data-api/tests/test_snapshots.py
+++ source-google-analytics-data-api/tests/test_snapshots.py
@@ -4,21 +4,21 @@ import subprocess
 import pytest_insta.format as insta_format
 insta_format.FmtJson.dump = lambda _self, path, value: path.write_text(json.dumps(value, sort_keys=True, indent=2) + "\n", "utf-8")
 
-# def test_capture(request, snapshot):
-#     result = subprocess.run(
-#         [
-#             "flowctl",
-#             "preview",
-#             "--source",
-#             request.fspath.dirname + "/../test.flow.yaml",
-#         ],
-#         stdout=subprocess.PIPE,
-#         text=True,
-#     )
-#     assert result.returncode == 0
-#     lines = [json.loads(l) for l in result.stdout.splitlines()]
+def test_capture(request, snapshot):
+    result = subprocess.run(
+        [
+            "flowctl",
+            "preview",
+            "--source",
+            request.fspath.dirname + "/../test.flow.yaml",
+        ],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    assert result.returncode == 0
+    lines = [json.loads(l) for l in result.stdout.splitlines()]
 
-#     assert snapshot("capture.stdout.json") == lines
+    assert snapshot("capture.stdout.json") == lines
 
 def test_discover(request, snapshot):
     result = subprocess.run(
